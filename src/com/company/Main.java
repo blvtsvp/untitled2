cc4d74630a5c52d2c6b44e89782d9c044cf763d5
blvtsvp <polina236@mail.ru>
2020-12-10 17:10:35 +0300

    first pr

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int z = 0;
+	System.out.println("Hello, world");
+
+    }
+}
